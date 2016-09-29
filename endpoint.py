98e829bdf6dcedf0106fecdbbc2e5a7e072296f3
Sergei-Rudenkov <Sergei_Rudenkov@epam.com>
2016-09-29 14:57:18 +0300

    fix async, add enter event handler

--- endpoint.py
+++ endpoint.py
@@ -3,21 +3,34 @@ import sys
 import psycopg2
 from lxml import html
 import requests
+from tornado import gen
 
 
+'''Asynchronous handler for http requests and DB operations
+'''
 class LevelHandler(tornado.web.RequestHandler):
     def __init__(self, application, request, **kwargs):
         super().__init__(application, request, **kwargs)
         connection = self.settings['connection']
         self.cursor = connection.cursor()
 
+    '''
+        Base get method
+         - if word cached to DB - return word
+         - check that level defined at Cambridge dictionary
+         - find word by xpath in Cambridge dictionary
+    '''
+    @gen.coroutine
     def get(self, word):
-        page = requests.get('http://dictionary.cambridge.org/dictionary/english/%s' % word)
-        tree = html.fromstring(page.content)
-        is_word_cached = self.check_cache(word)
+        self.set_header("Access-Control-Allow-Origin", "*")
+        is_word_cached =  self.check_cache(word)
         if is_word_cached:
             response = {'level': is_word_cached[0][0], 'word': word}
-        elif self.check_word_404(tree):
+            self.write(response)
+            return
+        page = requests.get('http://dictionary.cambridge.org/dictionary/english/%s' % word)
+        tree = html.fromstring(page.content)
+        if self.check_word_404(tree):
             response = {'level': "This word was not found", 'word': word}
         else:
             try:
@@ -27,14 +40,20 @@ class LevelHandler(tornado.web.RequestHandler):
                 level = "The word level is not known"
             self.write_cache(word, level)
             response = {'level': level, 'word': word}
-        self.set_header("Access-Control-Allow-Origin", "*")
         self.write(response)
 
+    '''
+    isContains for Postgresql
+    '''
     def check_cache(self, word):
         self.cursor.execute("SELECT level FROM eng_level WHERE word = %s", (word,))
         records = self.cursor.fetchall()
         return records
 
+    '''
+    Write cache to Postgresql
+    '''
+    @gen.coroutine
     def write_cache(self, word, level):
         self.cursor.execute("INSERT INTO eng_level (word, level) values (%s, %s)", (word, level,))
         self.cursor.execute("COMMIT")
